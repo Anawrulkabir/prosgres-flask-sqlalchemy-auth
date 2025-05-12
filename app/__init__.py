939997d5b0887ff6e94c2238e077c039b2753fbe
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2025-05-12 12:33:12 +0600

    Some configuation changes in the environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 jwt = JWTManager()
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
 
     # Configurations
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://user:password@db:5432/auth_db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'your-secret-key')
+    app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600  # 1 hour
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'your-flask-secret-key')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     # Initialize extensions
     db.init_app(app)
